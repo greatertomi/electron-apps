b1d8b5beefb407c50052321db5a9e0903a2c6e70
Oshalusi John <oshalusijohn@gmail.com>
2021-01-18 16:23:59 +0100

    Worked on the menus

--- imageShrinker/main.js
+++ imageShrinker/main.js
@@ -1,19 +1,94 @@
-const electron = require('electron');
-const ffmpeg = require('fluent-ffmpeg');
+const { app, BrowserWindow, Menu } = require('electron')
+// const log = require('electron-log')
 
-const { app, BrowserWindow, ipcMain } = electron;
+// Set env
+process.env.NODE_ENV = 'development'
+
+const isDev = process.env.NODE_ENV !== 'production'
+const isMac = process.platform === 'darwin'
+
 let mainWindow;
+let aboutWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    title: 'Image Shrinker',
+    width: isDev ? 800 : 500,
+    height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev,
+    backgroundColor: 'white',
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  })
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools()
+  }
+
+  mainWindow.loadFile('index.html')
+}
+
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    title: 'Image Shrinker',
+    width: 300,
+    height: 300,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    backgroundColor: 'white'
+  })
+
+  aboutWindow.loadFile('about.html')
+}
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({});
-  mainWindow.loadFile('index.html');
-  console.log('App is now ready');
-});
+  createMainWindow()
 
-ipcMain.on('videoPath', (event, path) => {
-  /* ffmpeg.ffprobe(path, () => (err, metadata) => {
-    mainWindow.webContents.send('video:metadata', metadata.format.duration);
-  }); */
-  console.log('here');
-  mainWindow.webContents.send('video:metadata', 4.0);
-});
+  const mainMenu = Menu.buildFromTemplate(menu)
+  Menu.setApplicationMenu(mainMenu)
+  mainWindow.on('ready', () => mainWindow = null)
+})
+
+const menu = [
+  ...(isMac ? [{ role: 'appMenu' }] : []),
+  {
+    role: 'fileMenu',
+  },
+  {
+    label: 'Help',
+    submenu: [
+      {
+        label: 'About',
+        click: createAboutWindow
+      }
+    ]
+  },
+  ...(isDev
+    ? [
+      {
+        label: 'Developer',
+        submenu: [
+          { role: 'reload' },
+          { role: 'forcereload' },
+          { type: 'separator' },
+          { role: 'toggledevtools' },
+        ],
+      },
+    ]
+    : []),
+]
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow()
+  }
+})
+
+app.allowRendererProcessReuse = true
